f2c85e1ef33f8d6517319e12c9351be5925c0ffd
hrakad <h.alrakad82@hotmail.com>
2021-11-12 00:27:06 -0700

    Populate the Component by passing data to each <DayListItem> to create array of DayListItem components

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,18 @@ import React from 'react';
 import DayListItem from './DayListItem';
 
 export default function DayList(props) {
+  const DayListItems = props.days.map(day => {
+    return <DayListItem 
+    key={day.id}
+    name={day.name} 
+    spots={day.spots} 
+    selected={day.name === props.day}
+    setDay={props.setDay}  
+  />
+  })
   return (
-    <ul></ul>
+    <ul>
+      {DayListItems}
+    </ul>
   )
 };
