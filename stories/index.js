89344f429c41aff76c763bc4b3a98c6a48d9e2f6
hrakad <h.alrakad82@hotmail.com>
2021-11-14 22:26:38 -0700

    Update InterviewerList stories

--- stories/index.js
+++ stories/index.js
@@ -122,15 +122,23 @@ const interviewers = [
 //InterviewerList stories
 storiesOf("InterviewerList", module)
   .addParameters({
-    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
   })
-  .add("Initial", () => <InterviewerList interviewers={interviewers} />)
+  .add("Initial", () => (
+    <InterviewerList
+      interviewers={interviewers}
+    />
+  ))
   .add("Selected", () => (
-    <InterviewerList interviewers={interviewers} interviewer={3} />
+    <InterviewerList
+      interviewers={interviewers}
+      value={3}
+    />
   ))
   .add("Clickable", () => (
     <InterviewerList
       interviewers={interviewers}
-      setInterviewer={action("setInterviewer")}
+      onChange={action("setInterviewer")}
     />
   ));
+
