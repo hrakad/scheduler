bfaf45ce09d696e9969282957017c59852071393
hrakad <h.alrakad82@hotmail.com>
2021-11-18 02:32:45 -0700

    Add the Appointment views from Storybook to the Application

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import DayList from "./DayList";
+import Appointment from "./Appointment";
 
 import "components/Application.scss";
 
@@ -21,8 +22,57 @@ const days = [
   },
 ];
 
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+
+  const appointmentsArray = Object.values(appointments).map(appointment => {
+    return (
+      <Appointment
+        key={appointment.id}
+        {...appointment}
+      />
+    )
+  })
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -47,8 +97,48 @@ export default function Application(props) {
         {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentsArray}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
 }
+
+
+// export default function Application(props) {
+
+//   // states
+//   //setDay state, to be passed down to DayListItem
+//   const [day, setDay] = useState("Monday");
+
+
+
+//   return (
+//     <main className="layout">
+//       <section className="sidebar">
+//         <img
+//           className="sidebar--centered"
+//           src="images/logo.png"
+//           alt="Interview Scheduler"
+//         />
+//         <hr className="sidebar__separator sidebar--centered" />
+//         <nav className="sidebar__menu">
+//           <DayList
+//             days={days}
+//             value={day}
+//             onChange={setDay}
+//           />
+//         </nav>
+//         <img
+//           className="sidebar__lhl sidebar--centered"
+//           src="images/lhl.png"
+//           alt="Lighthouse Labs"
+//         />
+//       </section>
+//       <section className="schedule">
+//         {appointmentsArray}
+//         <Appointment key="last" time="5pm" />
+//       </section>
+//     </main>
+//   );
+// }
