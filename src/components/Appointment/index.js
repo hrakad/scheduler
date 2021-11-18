1e75efb43ad1f0e6ca1859afa2ca0d60b190f3aa
hrakad <h.alrakad82@hotmail.com>
2021-11-18 00:01:39 -0700

    import components to Appointment/index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
+
+import "./styles.scss";
 
 export default function Appointment(props) {
   return (
