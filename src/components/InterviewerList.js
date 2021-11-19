2b40d30cc0f21a175185b57735f699f8c3d6ade1
hrakad <h.alrakad82@hotmail.com>
2021-11-18 23:32:40 -0700

    Install axios library where we'll use it to make requests in the app

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,13 +5,13 @@ import "components/InterviewerList.scss"
 
 export default function InterviewerList(props) {
   const interviewersList = props.interviewers.map(interviewer => {
-    return <InterviewerListItem 
-    key={interviewer.id}
-    name={interviewer.name} 
-    avatar={interviewer.avatar} 
-    selected={interviewer.id === props.value}
-    setInterviewer={() => props.onChange(interviewer.id)}
-  />
+    return <InterviewerListItem
+      key={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={interviewer.id === props.value}
+      setInterviewer={() => props.onChange(interviewer.id)}
+    />
   });
   return (
     <section className="interviewers">
