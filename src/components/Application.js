5bb0a633ef0b3bd65c251abc1565c052fb8dd72f
hrakad <h.alrakad82@hotmail.com>
2021-11-20 12:48:08 -0700

    Use axios and useEffect to request data from scheduler-api

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,23 +6,6 @@ import Appointment from "./Appointment";
 
 import "components/Application.scss";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -64,7 +47,14 @@ const appointments = {
 };
 
 export default function Application(props) {
+  const [days, setDays] = useState([]);
+
   const [day, setDay] = useState("Monday");
+
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days")
+      .then((response) => (setDays([...response.data])))
+  }, [])
 
   const appointmentsArray = Object.values(appointments).map(appointment => {
     return (
@@ -105,42 +95,3 @@ export default function Application(props) {
     </main>
   );
 }
-
-
-// export default function Application(props) {
-
-//   // states
-//   //setDay state, to be passed down to DayListItem
-//   const [day, setDay] = useState("Monday");
-
-
-
-//   return (
-//     <main className="layout">
-//       <section className="sidebar">
-//         <img
-//           className="sidebar--centered"
-//           src="images/logo.png"
-//           alt="Interview Scheduler"
-//         />
-//         <hr className="sidebar__separator sidebar--centered" />
-//         <nav className="sidebar__menu">
-//           <DayList
-//             days={days}
-//             value={day}
-//             onChange={setDay}
-//           />
-//         </nav>
-//         <img
-//           className="sidebar__lhl sidebar--centered"
-//           src="images/lhl.png"
-//           alt="Lighthouse Labs"
-//         />
-//       </section>
-//       <section className="schedule">
-//         {appointmentsArray}
-//         <Appointment key="last" time="5pm" />
-//       </section>
-//     </main>
-//   );
-// }
