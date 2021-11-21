c951abd88f11e5995b28a1a5ec0c10f4c13b4638
hrakad <h.alrakad82@hotmail.com>
2021-11-21 16:22:46 -0700

    Update the props being passed to the DayList component to access the day and days properties from within the state object

--- src/components/Application.js
+++ src/components/Application.js
@@ -78,8 +78,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onCharge={setDay}
           />
         </nav>
