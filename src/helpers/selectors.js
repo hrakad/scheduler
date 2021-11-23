3748fd4d1461436e127170acc6471bc88e2e702e
hrakad <h.alrakad82@hotmail.com>
2021-11-23 16:40:40 -0700

    Create a getInterview selector function which return a new object containing the interview data when we pass it an object that contains the interviewer

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,4 @@
+//getAppointmentsForDay helper function
 export function getAppointmentsForDay(state, day) {
   const filteredDays = state.days.filter(days => days.name === day);
   if (!filteredDays[0]) {
@@ -8,4 +9,18 @@ export function getAppointmentsForDay(state, day) {
   });
 
   return filteredAppointments;
-}
+};
+
+
+//getInterview helper function
+export function getInterview(state, interview) {
+
+  if (!interview) {
+    return null;
+  }
+  const interviews = {
+    student: interview.student,
+    interviewer: state.interviewers[interview.interviewer]
+  };
+  return interviews;
+};
