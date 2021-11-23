1c2f9db9ffbf3c5d7ecc72bc9fccc6140065e990
hrakad <h.alrakad82@hotmail.com>
2021-11-22 23:22:40 -0700

    Use promise.all to make the days and appointments requests before updating the status and display the appointments by use the helper function getAppointmentsForDay

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,6 +5,7 @@ import DayList from "./DayList";
 import Appointment from "./Appointment";
 
 import "components/Application.scss";
+import { getAppointmentsForDay } from '../helpers/selectors';
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -13,12 +14,21 @@ export default function Application(props) {
     appointments: {}
   });
 
-  const dailyAppointments = [];
+  const dailyAppointments = getAppointmentsForDay(state, state.day)
   const setDay = day => setState({ ...state, day });
 
   useEffect(() => {
-    axios.get("http://localhost:8001/api/days")
-      .then((response) => (setDays(response.data)))
+    Promise.all([
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments")
+    ])
+      .then((all) => {
+        setState((prev) => ({
+          ...prev,
+          days: all[0].data,
+          appointments: all[1].data,
+        }))
+      });
   }, [])
 
   const appointmentsArray = dailyAppointments.map(appointment => {
