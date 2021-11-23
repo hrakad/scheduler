c39c9e77b71e58ad2bcc5cffbfe7ab67766bc4ae
hrakad <h.alrakad82@hotmail.com>
2021-11-23 00:21:15 -0700

    Fetching interviewers data by modifying the effect method to do a third request using Promise.all and update the interviewers state with the response

--- src/components/Application.js
+++ src/components/Application.js
@@ -21,17 +21,21 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments")
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers")
     ])
       .then((all) => {
         setState((prev) => ({
           ...prev,
           days: all[0].data,
           appointments: all[1].data,
+          interviewers: all[2].data,
         }))
       });
   }, [])
-
+  // console.log(state.days);
+  // console.log(state.appointments);
+  // console.log(state.interviewers);
   const appointmentsArray = dailyAppointments.map(appointment => {
     return (
       <Appointment
