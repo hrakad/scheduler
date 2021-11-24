356b990cd8c37dfb6417dd5035c502218a90bba0
hrakad <h.alrakad82@hotmail.com>
2021-11-24 15:48:10 -0700

    Create transition function and back function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,5 +2,22 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-  return { mode }
+  const [history, setHistory] = useState([initial]);
+  function transition(mode) {
+    //console.log(mode)
+    setMode(mode);
+
+    setHistory(() => {
+      history.push(mode);
+      return history;
+    });
+    //console.log(history)
+  }
+
+  function back() {
+    history.pop();
+    setMode(history[history.length - 1]);
+  }
+
+  return { mode, transition, back };
 }
