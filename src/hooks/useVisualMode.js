df00a12f71be99084cbaee8cda80d4f8440cf7f4
hrakad <h.alrakad82@hotmail.com>
2021-11-24 16:45:03 -0700

    Modify transition function by adding replace case use it to decide the transition behaviour

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,20 +3,23 @@ import { useState } from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-  function transition(mode) {
-    //console.log(mode)
-    setMode(mode);
+  function transition(mode, replace) {
+    if (!replace) {
+      setMode(mode);
 
-    setHistory(() => {
+      setHistory(() => {
+        history.push(mode);
+        return history;
+      });
+    } else {
+      history.pop();
       history.push(mode);
-      return history;
-    });
-    //console.log(history)
+      setMode(history[history.length - 1]);
+    }
   }
 
   function back(mode) {
     if (history.length > 1) {
-      // console.log(history)
       history.pop();
       setMode(history[history.length - 1]);
     }
