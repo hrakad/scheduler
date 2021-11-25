23901032e820193330cea1144f3b3104d6023a1f
hrakad <h.alrakad82@hotmail.com>
2021-11-24 19:25:57 -0700

    Import useVisualMode hook to determine the mode, add the mode constants and update the to render a component conditionally when the mode matches

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,15 +3,29 @@ import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
 
+import useVisualMode from 'hooks/useVisualMode';
+
 import "./styles.scss";
 
 export default function Appointment(props) {
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
       <Header
         time={props.time}>
       </Header>
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
     </article>
   )
 }
