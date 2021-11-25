90e6f751b9f494e9dc2cd60a88f47063806cb5f7
hrakad <h.alrakad82@hotmail.com>
2021-11-24 20:25:28 -0700

    Refactor the code by add onCancle method to the danger button directly

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,15 +6,6 @@ export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
-  const reset = () => {
-    setStudent = ("")
-    setInterviewer = (null)
-  }
-
-  const cancel = () => {
-    reset()
-    props.onCancel()
-  }
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -37,7 +28,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={() => cancel(student, interviewer)}>Cancel</Button>
+          <Button danger onClick={() => props.onCancel()}>Cancel</Button>
           <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
         </section>
       </section>
