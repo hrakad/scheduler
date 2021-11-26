727c87daafaa8e22a35ff8fc04caf779baa607e2
hrakad <h.alrakad82@hotmail.com>
2021-11-26 00:30:47 -0700

    Implementing the update by creating a new appointment object starting with the values copied from the existing appointment, updating the appointments object, and calling the setState with the new state object

--- src/components/Application.js
+++ src/components/Application.js
@@ -35,7 +35,19 @@ export default function Application(props) {
   // console.log(state.interviewers);
 
   function bookInterview(id, interview) {
-    console.log(id, interview);
+    //console.log(id, interview);
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    setState({
+      ...state,
+      appointments
+    });
   }
 
   const setDay = day => setState({ ...state, day });
