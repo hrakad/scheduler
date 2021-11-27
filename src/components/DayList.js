5dde3cfca427133b14c968cab84f72809a23e424
hrakad <h.alrakad82@hotmail.com>
2021-11-27 16:40:40 -0700

    Fix typo

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,13 +3,13 @@ import DayListItem from './DayListItem';
 
 export default function DayList(props) {
   const DayListItems = props.days.map(day => {
-    return <DayListItem 
-    key={day.id}
-    name={day.name} 
-    spots={day.spots} 
-    selected={day.name === props.value}
-    setDay={() => props.onCharge(day.name)}  
-  />
+    return <DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.value}
+      setDay={() => props.onChange(day.name)}
+    />
   })
   return (
     <ul>
