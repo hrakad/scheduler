5cd24d794d6c65bae91840809943c93bbf6db061
hrakad <h.alrakad82@hotmail.com>
2021-11-30 23:55:36 -0700

    Implement error handling to display error messages to user on server

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,11 +5,13 @@ import Empty from './Empty';
 import Form from './Form';
 import Status from './Status';
 import Confirm from './Confirm';
+import Error from './Error';
 
 import useVisualMode from 'hooks/useVisualMode';
 
 import "./styles.scss";
 
+/************** Constants **************/
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREAT";
@@ -17,6 +19,8 @@ const SAVING = "SAVING";
 const DELETING = "DELETING";
 const CONFIRM = "CONFIRM";
 const EDIT = "EDIT";
+const ERROR_SAVE = "ERROR_SAVE";
+const ERROR_DELETE = "ERROR_DELETE";
 
 export default function Appointment(props) {
 
@@ -35,14 +39,21 @@ export default function Appointment(props) {
       .then(() => {
         transition(SHOW)
       })
+      .catch(() => {
+        transition(ERROR_SAVE, true)
+      })
   }
 
 
   const deleteInterview = () => {
     transition(DELETING);
-    props.deleteInterview(props.id).then(() => {
-      transition(EMPTY);
-    });
+    props.deleteInterview(props.id)
+      .then(() => {
+        transition(EMPTY);
+      })
+      .catch(() => {
+        transition(ERROR_DELETE, true)
+      })
   };
 
   const cancel = () => {
@@ -60,7 +71,6 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-
           deleteInterview={() => transition(CONFIRM)}
           editInterview={() => transition(EDIT)}
         />
@@ -91,7 +101,12 @@ export default function Appointment(props) {
 
       {mode === SAVING && <Status message="Saving" />}
       {mode === DELETING && <Status message="Deleting" />}
-
+      {mode === ERROR_DELETE && (
+        <Error message="Could not cancel Appointment" onCancel={() => back()} />
+      )}
+      {mode === ERROR_SAVE && (
+        <Error message="Could not save Appointment" onCancel={() => back()} />
+      )}
 
     </article>
   )
