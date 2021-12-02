b51b22b7b582a8adaed9bf4716c723530483805b
hrakad <h.alrakad82@hotmail.com>
2021-12-01 20:09:57 -0700

    Create custom hook and move the logic used to manage the state from Application.js to it

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useApplicationData() {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+
+  useEffect(() => {
+    Promise.all([
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers")
+    ])
+      .then((all) => {
+        setState((prev) => ({
+          ...prev,
+          days: all[0].data,
+          appointments: all[1].data,
+          interviewers: all[2].data,
+        }))
+      });
+  }, [])
+  // console.log(state.days);
+  // console.log(state.appointments);
+  // console.log(state.interviewers);
+
+  function bookInterview(id, interview) {
+    //console.log(id, interview);
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    const updateState = setState({
+      ...state,
+      appointments
+    });
+    return axios.put(`http://localhost:8001/api/appointments/${id}`, { interview })
+      .then((res) => {
+        if (res.headers.status === 204) updateState();
+      })
+  }
+
+  const deleteInterview = (id) => {
+    const updateState = setState({
+      ...state,
+      appointments: {
+        ...state.appointments,
+        [id]: { ...state.appointments[id], interview: null },
+      },
+    });
+    return axios
+      .delete(`http://localhost:8001/api/appointments/${id}`)
+      .then((res) => {
+        if (res.headers.status === 200) updateState();
+      });
+  };
+
+  return { setDay, state, bookInterview, deleteInterview };
+}
