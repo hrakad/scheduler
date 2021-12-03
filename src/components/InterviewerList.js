c204dbbbe8a7e5125432672e079fc875567efdfc
hrakad <h.alrakad82@hotmail.com>
2021-12-03 00:12:04 -0700

    Import prop-types library and add code to validate the interviewers props

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import InterviewerListItem from './InterviewerListItem';
+
 
 import "components/InterviewerList.scss"
 
@@ -22,3 +24,7 @@ export default function InterviewerList(props) {
     </section>
   )
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
