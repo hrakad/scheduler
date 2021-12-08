aa9a6f64ed707144e1bbd0d513fe314316c8b6ef
hrakad <h.alrakad82@hotmail.com>
2021-12-07 23:33:10 -0700

    Create validate function in the body of the Form component and add error state

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,16 @@ import Button from 'components/Button';
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState('');
+
+  const validate = () => {
+    if (student === '') {
+      setError('Student name cannot be blank');
+      return;
+    }
+
+    props.onSave(student, interviewer);
+  };
 
 
   return (
@@ -18,8 +28,10 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={student}
             onChange={(event) => setStudent(event.target.value)}
+            data-testid="student-name-input"
           />
         </form>
+        <section className='appointment__validation'>{error}</section>
         <InterviewerList
           interviewers={props.interviewers}
           value={interviewer}
@@ -29,7 +41,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={() => props.onCancel()}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(student, interviewer, props.newRequest)}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
