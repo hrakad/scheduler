714cdaf2a8e23c3c456ca0f486869306d59590d0
hrakad <h.alrakad82@hotmail.com>
2021-12-13 01:32:00 -0700

    Add tests to Editing an interviewand and keep the spots remaining for Monday the same

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -75,7 +75,7 @@ describe("Application", () => {
 
     fireEvent.click(queryByAltText(appointment, "Delete"));
 
-    //   // 4. Check that the confirmation message is shown.
+    // 4. Check that the confirmation message is shown.
     expect(
       getByText(appointment, "Delete the appointment?")
     ).toBeInTheDocument();
@@ -96,4 +96,41 @@ describe("Application", () => {
 
     expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
   });
+
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    // 1. Render the Application.
+    const { container } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    // 3. Click the "Edit" button.
+    const appointment = getAllByTestId(container, "appointment").find(
+      (appointment) => queryByText(appointment, "Archie Cohen")
+    );
+
+    fireEvent.click(queryByAltText(appointment, "Edit"));
+
+    // 4. Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    // 5. Click the "Save" button on that same appointment.
+    fireEvent.click(getByText(appointment, "Save"));
+
+    // 6. Check that the element with the text "Saving" is displayed.
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    // 8. Wait until the element with the text "Lydia Miller-Jones" is displayed.
+    await waitForElement(() => getByText(container, "Lydia Miller-Jones"));
+
+    // 8. Check that the DayListItem with the text "Monday" also has the text "1 spot remaining".
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+
+    expect(getByText(day, "1 spot remaining"));
+  });
+
 });
