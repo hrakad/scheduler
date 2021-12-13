847c619685e15cf655f7304ad8ce981df337a586
hrakad <h.alrakad82@hotmail.com>
2021-12-13 11:38:19 -0700

    Add saving error test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 import { render, cleanup, waitForElement, fireEvent, prettyDOM } from "@testing-library/react";
 import { getByText, getAllByTestId, getByPlaceholderText, getByAltText, queryByText, queryByAltText } from "@testing-library/react";
@@ -23,6 +24,7 @@ describe("Application", () => {
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
 
+  //*********** Booking test ************//
   it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
     // 1. Render the Application.
     const { container, debug } = render(<Application />);
@@ -61,6 +63,7 @@ describe("Application", () => {
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
 
+  //************ Canceling test ************//
   it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
     // 1. Render the Application.
     const { container } = render(<Application />);
@@ -97,6 +100,7 @@ describe("Application", () => {
     expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
   });
 
+  //************ Editing test ************//
   it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
     // 1. Render the Application.
     const { container } = render(<Application />);
@@ -131,6 +135,49 @@ describe("Application", () => {
     );
 
     expect(getByText(day, "1 spot remaining"));
+  });
+
+  //************  SAVING ERROR test ************//
+  it("shows the save error when failing to save an appointment", async () => {
+    axios.put.mockRejectedValueOnce();
+
+    // 1. Render the Application.
+    const { container } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    // get all the appointments using the test ID
+    const appointments = getAllByTestId(container, "appointment");
+
+    // get the first empty appointment
+    const appointment = appointments[0];
+
+    // 3. Click the "Add" button on the first empty appointment.
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    // 4. Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    // 5. Click the first interviewer in the list.
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    // 6. Click the "Save" button on that same appointment.
+    fireEvent.click(getByText(appointment, "Save"));
+
+    // 7. Wait to get the error 
+    await waitForElement(() => getByText(appointment, "Error"));
+
+    expect(getByText(appointment, "Error")).toBeInTheDocument();
+
+    // 8. Check that spots still display "1 spot remaining".
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+
+    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
   });
 
 });
