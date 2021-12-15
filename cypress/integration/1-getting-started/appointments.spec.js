28b600af64f346f558811cc9b659c549708e1482
hrakad <h.alrakad82@hotmail.com>
2021-12-14 19:05:29 -0700

    Add cypress test to test editing an interview

--- cypress/integration/1-getting-started/appointments.spec.js
+++ cypress/integration/1-getting-started/appointments.spec.js
@@ -7,6 +7,7 @@ describe("Appointments", () => {
     cy.contains("Monday");
   });
 
+  //Book an interview test
   it("should book an interview", () => {
     cy.get("[alt=Add]")
       .first()
@@ -19,5 +20,20 @@ describe("Appointments", () => {
 
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+  //Edit an interview test
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true });
+
+    cy.get("[data-testid=student-name-input]").clear().type("Adam Smith");
+    cy.get('[alt="Tori Malcolm"]').click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Adam Smith");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
   });
 });
