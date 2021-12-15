f2ffa58f3f32f8282d28761c71bce8ab5707f112
hrakad <h.alrakad82@hotmail.com>
2021-12-14 17:29:58 -0700

    add cypress tests to check day list component's css

--- cypress/integration/1-getting-started/navigation.spec.js
+++ cypress/integration/1-getting-started/navigation.spec.js
@@ -2,4 +2,13 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+
+  it("Should navigate to Tuseday", () => {
+    cy.visit("/");
+    cy.contains("li", "Tuesday")
+      .click()
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
+
+  });
+
 });
